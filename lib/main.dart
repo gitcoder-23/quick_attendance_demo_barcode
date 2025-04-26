78da8cebc5c25324618cc904c9ecbf794b50e94c
gitcoder-23 <prabhatece21@gmail.com>
2025-04-26 18:57:36 +0530

    name added

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Quick Attendance',
+      title: 'Hazira Sathi',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
